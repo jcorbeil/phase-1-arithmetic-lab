fec8ef841ef39f187f8a27e5e6381b92c7c75473
Jeremy Corbeil <jeremycorbeil9@yahoo.com>
2023-06-06 01:38:51 -0400

    finished-phase-1-arithetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+
+let set = [10, 5, 20, 15];
+let max = Math.max(...set);
